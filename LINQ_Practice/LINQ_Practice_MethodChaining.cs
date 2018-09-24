abf6fb1beaa31963cc5831b82ba747b36da3f011
Amanda Slayton <alslayton86@gmail.com>
2018-09-24 17:20:33 -0500

    first part done

--- LINQ_Practice/LINQ_Practice_MethodChaining.cs
+++ LINQ_Practice/LINQ_Practice_MethodChaining.cs
@@ -29,7 +29,7 @@ namespace LINQ_Practice
         [TestMethod]
         public void GetAllCohortsWithZacharyZohanAsPrimaryOrJuniorInstructor()
         {
-            var ActualCohorts = PracticeData/*FILL IN LINQ EXPRESSION*/.ToList();
+            var ActualCohorts = PracticeData.Where(cohort => cohort.PrimaryInstructor.LastName == "Zohan" || cohort.JuniorInstructors.Any(instructor => instructor.LastName == "Zohan")).ToList();
             CollectionAssert.AreEqual(ActualCohorts, new List<Cohort> { CohortBuilder.Cohort2, CohortBuilder.Cohort3 });
         }
 
